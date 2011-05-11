0153fcc7c8deb70ff69284cb0982c3b6e4f1549c
christos.sioutis@gmail.com <christos.sioutis@gmail.com@b26b4685-48d9-d157-aa34-9c68780e9561>
2011-05-11 11:22:24 +0000

    added rtps_types.idl from rtjdds in order to get the code to compile, will merge these with dds_rtps.idl in the RTPS namespace
    git-svn-id: https://jdds.googlecode.com/svn/trunk@4 b26b4685-48d9-d157-aa34-9c68780e9561

--- JDDS/makeIDL.sh
+++ JDDS/makeIDL.sh
@@ -3,3 +3,5 @@ echo idlj dds_dcps.idl
 idlj  -td ./idlgen ./idl/dds_dcps.idl
 echo idlj dds_rtps.idl
 idlj  -td ./idlgen -i ./idl ./idl/dds_rtps.idl
+echo idlj rtps_types.idl
+idlj  -td ./idlgen -i ./idl ./idl/rtps_types.idl
