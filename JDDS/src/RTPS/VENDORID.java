67a1bf826213b341c506c13558dfb81ae27b7c6a
Christos Sioutis <christos.sioutis@gmail.com>
2013-11-09 16:10:30 +1030

    tweaking

--- JDDS/src/RTPS/VENDORID.java
+++ JDDS/src/RTPS/VENDORID.java
@@ -26,43 +26,54 @@
 
 package RTPS;
 
-//OCT 2010 http://www.omgwiki.org/dds/content/page/dds-rtps-vendor-ids
-//#define VENDORID_RTI {1,1}
-//#define VENDORID_PRISMTECH {1,2}
-//#define VENDORID_OCI {1,3}
-//#define VENDORID_MILSOFT {1,4}
-//#define VENDORID_KONGSBERG {1,5}
-//#define VENDORID_TWINOAKS {1,6}
-//#define VENDORID_LAKOTA {1,7}
-//#define VENDORID_ICOUP {1,8}
-//need to apply to OMG for a proper Vendor ID
-//#define VENDORID_JDDS {1,20}
+// Nov 2013
+// http://portals.omg.org/dds/content/page/dds-rtps-vendor-and-product-ids
+
+// if this starts working, should apply to OMG for a proper Vendor ID
 
 public interface VENDORID {
-	public static byte[] rawRTI 		= {0,1};
+	public static byte[] rawRTI 		= {1,1};
 	public static VendorId_t RTI = new VendorId_t(rawRTI);
 	
-	public static byte[] rawPrismTech 	= {0,2};
+	public static byte[] rawPrismTech 	= {1,2};
 	public static VendorId_t PrismTech = new VendorId_t(rawPrismTech);
 	
-	public static byte[] rawOCI 		= {0,3};
+	public static byte[] rawOCI 		= {1,3};
 	public static VendorId_t OCI = new VendorId_t(rawOCI);
 	
-	public static byte[] rawMilSoft 	= {0,4};
+	public static byte[] rawMilSoft 	= {1,4};
 	public static VendorId_t MilSoft = new VendorId_t(rawMilSoft);
 	
-	public static byte[] rawKongsberg 	= {0,5};
-	public static VendorId_t Kongsberg = new VendorId_t(rawKongsberg);
+	public static byte[] rawGallium 	= {1,5};
+	public static VendorId_t Gallium = new VendorId_t(rawGallium);
 	
-	public static byte[] rawTwinOaks 	= {0,6};
+	public static byte[] rawTwinOaks 	= {1,6};
 	public static VendorId_t TwinOaks = new VendorId_t(rawTwinOaks);
 	
-	public static byte[] rawLakota 		= {0,7};
+	public static byte[] rawLakota 		= {1,7};
 	public static VendorId_t Lakota = new VendorId_t(rawLakota);
 	
-	public static byte[] rawIcoup 		= {0,8};
+	public static byte[] rawIcoup 		= {1,8};
 	public static VendorId_t Icoup = new VendorId_t(rawIcoup);
+
+	public static byte[] rawETRI 		= {1,9};
+	public static VendorId_t ETRI = new VendorId_t(rawETRI);
+
+	public static byte[] rawRTIMicro	= {1,0xA};
+	public static VendorId_t RTIMicro = new VendorId_t(rawRTIMicro);
 	
+	public static byte[] rawPrismTechMobile	= {1,0xB};
+	public static VendorId_t PrismTechMobile = new VendorId_t(rawPrismTechMobile);
+	
+	public static byte[] rawPrismTechGateway	= {1,0xC};
+	public static VendorId_t PrismTechGateway = new VendorId_t(rawPrismTechGateway);
+	
+	public static byte[] rawPrismTechLite	= {1,0xD};
+	public static VendorId_t PrismTechLite = new VendorId_t(rawPrismTechLite);
+	
+	public static byte[] rawTechnicolor	= {1,0xE};
+	public static VendorId_t Technicolor = new VendorId_t(rawTechnicolor);
+
 	public static byte[] rawJDDS 		= {0,20};
 	public static VendorId_t JDDS = new VendorId_t(rawJDDS);
 }
